e269150ee05f00437ce87e0e2c2f8bf57493abf2
jamlmao <jajamagustin0624@gmail.com>
2025-01-05 21:23:57 +0800

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Personal from './components/Personal'
+function App() {
+  return (
+    <>
+      <Personal />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Personal.jsx
@@ -0,0 +1,86 @@
+import { useState } from "react";
+import * as Styles from './styles/Content.module.css'
+
+function Personal(){
+
+    const [isEditing, setIsEditing] = useState(false);
+    const [personalInfo, setPersonalInfo]=useState({
+        name: "John Doe",
+        address: "Home or Campus Street Address",
+        city: "City, State Zip",
+        email: "youremail@domain.com",
+        socials: "@socials",
+        phone: "xxxx-xxx-xxxx" 
+    });
+
+    const handleEdit =()=>{
+        setIsEditing(!isEditing);
+    };
+
+
+    const handleChange = (e) => { 
+        setPersonalInfo({
+            ...personalInfo,[e.target.name]: e.target.value
+        });
+    };
+
+
+
+    return(
+
+        <>
+            <div className={Styles.personalContainer}>
+                <button 
+                    onClick={handleEdit}
+                    className={Styles.editButton}
+                >
+                    {isEditing ? 'Save' : 'Edit'}
+                    </button>
+            
+        {isEditing?(
+            <>
+                <input type="text" name="name" value={personalInfo.name} 
+                onChange={(e) => handleChagne(e, 'name')} 
+                className={`${Styles.textCenter} ${Styles.name}`} />
+                <hr />
+                <ul className={`${Styles.flex} ${Styles.personal}`}>
+                    <li>
+                        <input type="text" name="address" value={personalInfo.address} onChange={(e) => handleChange(e, 'address')} className={`${Styles.textCenter} ${Styles.address}`} />
+                    </li>
+                    <li>
+                        <input type="text" name="city" value={personalInfo.city} onChange={(e) => handleChange(e, 'city')} className={`${Styles.textCenter} ${Styles.city}`} />
+                    </li>
+                    <li>
+                        <input type="text" name="email" value={personalInfo.email} onChange={(e) => handleChange(e, 'email')} className={`${Styles.textCenter} ${Styles.email}`} />
+                    </li>
+                    <li>
+                        <input type="text" name="socials" value={personalInfo.socials} onChange={(e) => handleChange(e, 'socials')} className={`${Styles.textCenter} ${Styles.socials}`} />
+                    </li>
+                    <li>
+                        <input type="text" name="phone" value={personalInfo.phone} onChange={(e) => handleChange(e, 'phone')} className={`${Styles.textCenter} ${Styles.phone}`} />
+                    </li>
+                </ul>
+            </>
+        ):(
+            <>
+                <h1 className={`${Styles.textCenter} ${Styles.name}`}>{personalInfo.name}</h1>
+                <hr />
+                <ul className={`${Styles.flex} ${Styles.personal}`}>
+                    <li className={Styles.address}>{personalInfo.address}</li>
+                    <li className={Styles.city}>•{personalInfo.city}</li>
+                    <li className={Styles.email}>•{personalInfo.email}</li>
+                    <li className={Styles.socials}>•{personalInfo.socials}</li>
+                    <li className={Styles.phone}>•{personalInfo.phone}</li>
+                </ul>
+            </>
+        )}
+          </div>
+       </>
+
+    )
+}
+
+
+
+
+export default Personal
